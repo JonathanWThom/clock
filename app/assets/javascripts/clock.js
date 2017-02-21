46eebaf168f185b9a07af3450a5e4238dd1987f4
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-02-20 22:13:36 -0800

    Clock time formatting

--- app/assets/javascripts/clock.js
+++ app/assets/javascripts/clock.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
   setInterval(function(){
-    $('#hours').text(new Date().getHours());
+    var hours = new Date().getHours();
+    if (hours > 12) {
+      hours -= 12
+    }
+    $('#hours').text(hours);
     $('#minutes').text(new Date().getMinutes());
     $('#seconds').text(new Date().getSeconds());
   }, 1000);
